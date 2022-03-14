62966430c5c5ca2eb532c5041137c80ba889f711
lkwebdev <lkwebdev@mail.ru>
2022-03-15 01:19:06 +0300

    ~

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/src/index.js', 'public/src')
     .react()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+
+    .copy('resources/images', 'public/images');
