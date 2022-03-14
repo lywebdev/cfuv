aa56fce8bca402c12b8f9f837763706ca4c97c1a
SarvarGulyaev <sarvaredition@yandex.ru>
2022-03-14 21:08:03 +0300

    done

--- resources/src/App.js
+++ resources/src/App.js
@@ -7,7 +7,7 @@ function App() {
                 <div className="col-md-8">
                     <div className="card">
                         <div className="card-header">Example Component</div>
-
+dasd
                         <div className="card-body">I'm an example component!</div>
                     </div>
                 </div>
